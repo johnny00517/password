d4f67adeb7729da8e77f904e27356cf88d88e9c7
Johnny Zhong <johnny00517@gmail.com>
2019-11-24 15:53:56 +0800

    000

--- password.py
+++ password.py
@@ -2,12 +2,16 @@ password = 'a123456'
 
 y = 3
 while y > 0:
+	y = y - 1
 	x = input('請輸入密碼: ')
 	if x == password:
 		print('登入成功')
 		break
 
-	else:
-		y = y - 1
-		print('密碼錯誤還有', y, '次機會')	
+	else:	
+		print('密碼錯誤!')
+		if y > 0:
+			print('還有', y, '次機會')
+		else:
+			print('沒機會了!!!!')
 			
